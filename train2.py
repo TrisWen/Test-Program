66842fa1b3ac782d92de5c05539a3a7fa5be2ba1
Tristan <tristanwennink@hotmail.nl>
2020-11-17 21:37:14 +0100

    train2 version2

--- train2.py
+++ train2.py
@@ -5,11 +5,23 @@ Created on Tue Nov 17 21:27:02 2020
 @author: Tristan
 """
 
-x = 3
-n = 10
+def function1(x,n):
+    for i in range(n):
+        x += 1
+    return x
 
-for i in range(n):
-    x += 1
+def function2(x,n):
+    for i in range(n):
+        x -= 1
+    return x
+
+def main():
+    x = function1(3,10)
+    y = function2(3,10)
+    print(x)
+    print(y)
+    mean = (x+y)/2
+    print("Mean is:", mean)       
     
-print(x)
-
+if __name__ == "__main__":  
+    main()
